02b55bd8c76a35e8eb68be157bb8967ff9d8a33a
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-06-24 22:16:13 +0600

    banner created

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Banner.css'
+import banner from '../../../images/bannernew.jpeg'
+import banner2 from '../../../images/section.png'
 const Banner = () => {
     return (
-        <div>
-            <h1>banner</h1>
-        </div>
+        <>
+            <div className='banner d-flex '>
+
+                <div className='text-align-center'><h1 className='banner-text text-align-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, ducimus, fugiat quis magnam commodi tempora, tenetur non atque optio deleniti labore sed! Voluptatibus commodi sapiente quisquam at cupiditate repudiandae unde dicta, deserunt expedita fuga repellat ducimus illum sit pariatur debitis beatae ullam quis reiciendis, esse ea officia! Iste, perspiciatis aut.</h1></div>
+                <img className='ms-auto' src={banner} alt="" />
+
+            </div>
+            {/* <div>
+                <img src={banner2} className='w-100' alt="" />
+            </div> */}
+        </>
     );
 };
 
